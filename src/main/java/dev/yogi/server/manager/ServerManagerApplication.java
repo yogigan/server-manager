7748110e238474f2bae7aefe0eedff8778cbda70
yogigan <bachtiarnuryogipratama@gmail.com>
2023-09-02 18:53:39 +0700

    initial commit

---
+++ src/main/java/dev/yogi/server/manager/ServerManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.yogi.server.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yogi/server/manager/ServerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yogi.server.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
